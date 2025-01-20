11728c12471925513d91c47d41d05d64fc7fbdf7
maris-baby-1999 <61262710+maris-baby-1999@users.noreply.github.com>
2025-01-20 11:51:17 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,8 @@
+// Create web server
+
+// Importing the express library
+const express = require('express');
+const app = express();
+
+// Importing the comments.json file
+const comments = require('./comments.json');
